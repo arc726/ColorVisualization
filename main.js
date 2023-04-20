5d5fb3e712168e94838853a253f5a74140416567
arc726 <Austin.R.Cassill@sherwin.com>
2023-04-20 19:19:51 -0400

    formatting

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 var scene = new THREE.Scene();
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+var camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  1,
+  1000
+);
 camera.position.set(0, 0, 5);
 scene.add(camera);
 
-const controls = new OrbitControls( camera, renderer.domElement );
+const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true; // enable smooth camera movement
 controls.dampingFactor = 0.05; // set damping factor
 controls.rotateSpeed = 0.5; // set rotate speed
 
 var loader = new GLTFLoader();
-loader.load('scene.gltf', function (gltf) {
-    gltf.scene.rotation.y = Math.PI / 2; 
-    gltf.scene.traverse(function (node) {
-        if (node.isMesh) {
-            if (node.name.includes("Carpaint_Main") && !node.name.includes("RIM")) {
-                // get the existing material
+loader.load("scene.gltf", function (gltf) {
+  gltf.scene.rotation.y = Math.PI / 2;
+  gltf.scene.traverse(function (node) {
+    if (node.isMesh) {
+      if (node.name.includes("Carpaint_Main") && !node.name.includes("RIM")) {
+        // get the existing material
         var material = node.material;
         // get the current color of the material
         var baseColor = material.color;
@@ -30,13 +35,11 @@ loader.load('scene.gltf', function (gltf) {
         var tintColor = new THREE.Color(0x00ff00); // set to green
         // multiply the base color and tint color to produce the new color
         material.color = baseColor.clone().multiply(tintColor);
-            }
-        }
-    });
-    
-    
-    
-    scene.add(gltf.scene);
+      }
+    }
+  });
+
+  scene.add(gltf.scene);
 });
 
 var ambientLight = new THREE.AmbientLight(0xffffff, 1);
@@ -49,8 +52,8 @@ scene.add(directionalLight);
 scene.background = new THREE.Color(0x999999);
 
 function animate() {
-    requestAnimationFrame(animate);
-    controls.update();
-    renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
 }
 animate();
